0e719abe63c6db936ce5c43652ab21bea2b2d3a9
step-code01 <stepcode01@gmail.com>
2025-07-04 23:45:02 +0530

    Update img_downloader3.sh

--- img_downloader3.sh
+++ img_downloader3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-LIMIT=10
+LIMIT=10 #Reddit's API only allows maximum 100 at a time, so be careful for that.
 
-RESPONSE=$(curl "https://www.reddit.com/r/pics/top/.json?t=all&limit=$LIMIT" -A "Mozilla/5.0")
+RESPONSE=$(curl "https://www.reddit.com/r/pics/top/.json?t=all&limit=$LIMIT" -A "Mozilla/5.0") 
 
 for i in $(seq 0 $((LIMIT-1))); do
     IMAGE_URL=$(echo "$RESPONSE" | jq -r ".data.children[$i].data.url_overridden_by_dest")
@@ -18,3 +18,7 @@ else
 fi
 
 done
+
+:'line 5, need to pretend to be a browser since reddit blocks unknown/empty user agent. 
+Makes reddit think request is coming from a legit browser.
+This makes the scraping possible.'
